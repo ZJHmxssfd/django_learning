6bfdf767d6bc7e5b055b739574dbd4d1fb59a70d
ZJHmxssfd <zjhmxssfd@gmail.com>
2020-05-27 22:39:44 +0800

    session会话

--- LRSystem/login/views.py
+++ LRSystem/login/views.py
@@ -4,16 +4,19 @@ from . import models
 from . import forms
 # Create your views here.
 def index(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
     return render(request, 'login/index.html')
 
 
 def login(request):
+    if request.session.get('is_login', None):   # 不允许重复登录
+        return redirect('/index/')
     if request.method == "POST":
         login_form = forms.UserForm(request.POST)
         message = '请检查填写的内容！'
         # 使用表单类自带的is_valid()方法一步完成数据验证工作
-        if login_form.isvalid():
+        if login_form.is_valid():
             # 验证成功后可以从表单对象的cleaned_data数据字典中获取表单的具体值
             username = login_form.cleaned_data.get('username')
             password = login_form.cleaned_data.get('password')
@@ -30,7 +33,9 @@ def login(request):
                 # 形如{'message': message, 'login_form': login_form}的字典了，但可能造成数据冗余降低效率
 
             if user.password == password:
-                # print(username, password)
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
                 return redirect('/index/')
             else:
                 message = '密码不正确！'
@@ -48,5 +53,13 @@ def register(request):
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/login/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    # flush()是比较安全的一种做法，不好之处是如果在session夹带了一点‘私货’，会被一并删除
     return redirect("/login/")
